f27439003b2a27597df341e8d70be2f395540d26
はねぺん <hanetaku0611@gmail.com>
2022-01-15 21:44:41 +0900

    1/15/21:45

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -4,15 +4,20 @@ using UnityEngine;
 
 public class GameManager : MonoBehaviour
 {
+    public GameObject _textGameover = null;
+    public GameObject _playerChara = null;
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        _textGameover.SetActive(false);
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        if(_playerChara.transform.position.y < -5.5f){
+            _textGameover.SetActive(true);
+        }
     }
 }
